54635031dc0fd4034ae47016786aa7862f7c6ab2
Renziito <sepia.aki@gmail.com>
2019-08-04 12:55:50 -0500

    change in databse

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,10 +2,10 @@
 
 return array(
     'class'              => 'CDbConnection',
-    'connectionString'   => 'mysql:host=localhost;dbname=db_sae;port=3306',
+    'connectionString'   => 'mysql:host=localhost;dbname=roberto_sae;port=3306',
     'emulatePrepare'     => true,
-    'username'           => 'userSAE',
-    'password'           => 'us3rs43',
+    'username'           => 'roberto_administracion',
+    'password'           => 'saedificio',
     'charset'            => 'utf8',
     'enableProfiling'    => true,
     'enableParamLogging' => true
