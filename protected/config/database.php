21b22a3afb0c47a488c2edde949d8dbd729be0fa
Renziito <sepia.aki@gmail.com>
2019-08-04 12:58:56 -0500

    databse

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
     'connectionString'   => 'mysql:host=localhost;dbname=roberto_sae;port=3306',
     'emulatePrepare'     => true,
     'username'           => 'roberto_administracion',
-    'password'           => 'saedificio',
+    'password'           => 'saedificio2019',
     'charset'            => 'utf8',
     'enableProfiling'    => true,
     'enableParamLogging' => true
